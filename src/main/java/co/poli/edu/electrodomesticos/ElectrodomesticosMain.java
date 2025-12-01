542e718eb815e5ab988c39cab4c6cfbd6440d7e4
Manuel Contreras Zarate <zaratejuanmanuel61@gmail.com>
2025-12-01 17:09:51 +0100

    proyecto poo electrodomesticos

---
+++ src/main/java/co/poli/edu/electrodomesticos/ElectrodomesticosMain.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.poli.edu.electrodomesticos;
+
+/**
+ *
+ * @author SALON206
+ */
+public class ElectrodomesticosMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
